c9e5449e380c56277869357bd96b9b9f45acb9a2
Patrick Gartung <gartung@fnal.gov>
2016-06-01 17:23:38 -0500

    bring these along too

---
+++ Modules/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/gartung/spack-stage/spack-stage-o1C2_6/fnal-canvas/Modules
+# Build directory: /tmp/gartung/spack-stage/spack-stage-o1C2_6/fnal-canvas/Modules
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
