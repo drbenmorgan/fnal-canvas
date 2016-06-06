4a1309a4a5990de8d74cbd72c7061e3d5d172d40
Patrick Gartung <gartung@fnal.gov>
2016-06-06 15:26:37 -0500

    removed FindSqlite3.cmake

--- Modules/altCMakeLists.cmake
+++ Modules/altCMakeLists.cmake
@@ -1,7 +1,6 @@
 INSTALL(FILES
   ArtDictionary.cmake
   FindCppUnit.cmake
-  FindSQLite3.cmake
   FindTBB.cmake
   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/canvas-${canvas_VERSION}
   COMPONENT Development
