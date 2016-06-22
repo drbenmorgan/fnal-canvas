a76b2749cc4a0567b85c059665882947c1517688
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-06-22 13:00:02 +0100

    Add tests for Persistency/Provenance
    Find CppUnit when testing is enabled as this is still required by
    some Persistency/Provenance tests. Add altCMakeLists file for
    the tests. Simplify link lists to minimum required to get tests
    linking with full symbol resolution.
    
    NB: Tests using runtime type demangling currently fail on Apple Clang
    due to inline namespace issue.

--- canvas/test/altCMakeLists.cmake
+++ canvas/test/altCMakeLists.cmake
@@ -1,10 +1,13 @@
+# Test still need CPPunit
+find_package(CppUnit REQUIRED)
+
 cet_enable_asserts()
 
 cet_test_env("FHICL_FILE_PATH=.")
 
 # build Persistency libraries
 add_subdirectory(Persistency/Common)
-#add_subdirectory(Persistency/Provenance)
+add_subdirectory(Persistency/Provenance)
 add_subdirectory(Version)
 add_subdirectory(Utilities)
 #add_subdirectory(tbb)
